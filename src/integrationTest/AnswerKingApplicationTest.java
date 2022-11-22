dd87f12e0393e396080b02876dfc0522bf7a7a3d
martintaylor1635 <martintaylor1635@gmail.com>
2022-11-22 10:17:29 +0000

    BENCH-194 Moved AbstractContainerBaseTest and AnswerKingApplicationTest into integrationTest directory

---
+++ src/integrationTest/AnswerKingApplicationTest.java
@@ -0,0 +1,14 @@
+package com.answerdigital.answerking;
+
+import com.answerdigital.answerking.utility.AbstractContainerBaseTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnswerKingApplicationTest extends AbstractContainerBaseTest
+{
+    @Test
+    void contextLoads(){
+
+    }
+}
